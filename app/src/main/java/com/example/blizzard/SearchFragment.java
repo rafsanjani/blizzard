856edfba071a4c21cff63643ce76c2c71205280e
Anthony <tonyqed@gmail.com>
2020-08-01 01:21:52 +0100

    layout edited

--- app/src/main/java/com/example/blizzard/SearchFragment.java
+++ app/src/main/java/com/example/blizzard/SearchFragment.java
@@ -9,15 +9,13 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
-public class SecondFragment extends Fragment {
+public class SearchFragment extends Fragment {
 
     @Override
     public View onCreateView(
-            LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState
-    ) {
+            LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false);
+        return inflater.inflate(R.layout.search_fragment, container, false);
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
@@ -26,7 +24,7 @@ public class SecondFragment extends Fragment {
         view.findViewById(R.id.button_second).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NavHostFragment.findNavController(SecondFragment.this)
+                NavHostFragment.findNavController(SearchFragment.this)
                         .navigate(R.id.action_SecondFragment_to_FirstFragment);
             }
         });
