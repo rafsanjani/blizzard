a53a5c630fec6352a175011a51553378686d08df
Anthony <tonyqed@gmail.com>
2020-08-01 11:57:07 +0100

    Update on layout

--- app/src/main/java/com/example/blizzard/MainActivity.java
+++ app/src/main/java/com/example/blizzard/MainActivity.java
@@ -21,15 +21,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
