4c7392d6ed72ba1e55ce0db842c320cbddd99691
Kelvin Clark <kelvinclark2k77@gmail.com>
2020-08-03 12:27:58 +0100

    added model for our data

--- app/src/main/java/com/example/blizzard/model/WeatherData.java
+++ app/src/main/java/com/example/blizzard/model/WeatherData.java
@@ -23,5 +23,69 @@ public class WeatherData {
     @SerializedName("temp")
     private String temperature;
 
+    public WeatherData(int id, String state, String description, String icon, String cityTitle, String humidity, String temperature) {
+        this.id = id;
+        this.state = state;
+        this.description = description;
+        this.icon = icon;
+        this.cityTitle = cityTitle;
+        this.humidity = humidity;
+        this.temperature = temperature;
+    }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public String getCityTitle() {
+        return cityTitle;
+    }
+
+    public void setCityTitle(String cityTitle) {
+        this.cityTitle = cityTitle;
+    }
+
+    public String getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(String humidity) {
+        this.humidity = humidity;
+    }
+
+    public String getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(String temperature) {
+        this.temperature = temperature;
+    }
 }
