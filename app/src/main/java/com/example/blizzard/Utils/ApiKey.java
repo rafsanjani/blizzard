85a62da81c1a4c1af124b7764c100acb58511421
Kelvin Clark <kelvinclark2k77@gmail.com>
2020-08-03 02:55:59 +0100

    added internet permission, added few classes to handle api call and the data model for the api call. And changed full backup permission to previous and add an xml file to fix the error. Added the class that holds the api key, but you have to paste you api key in the class for it to work.

---
+++ app/src/main/java/com/example/blizzard/Utils/ApiKey.java
@@ -0,0 +1,12 @@
+package com.example.blizzard.Utils;
+
+/**
+ * Created by kelvi on 8/3/2020
+ */
+public final class ApiKey {
+    private String api_key = "replace wit your api key";
+
+    public String getApi_key() {
+        return api_key;
+    }
+}
