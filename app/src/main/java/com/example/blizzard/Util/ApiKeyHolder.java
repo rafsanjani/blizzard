44fb54b76d9fd1df8bb054ca3bc0e853710d9382
Anthony <tonyqed@gmail.com>
2020-08-08 10:59:35 +0100

    update

--- app/src/main/java/com/example/blizzard/Util/ApiKeyHolder.java
+++ app/src/main/java/com/example/blizzard/Util/ApiKeyHolder.java
@@ -4,7 +4,7 @@ package com.example.blizzard.Util;
  * Created by kelvi on 8/3/2020
  */
 public final class ApiKeyHolder {
-    private final static String api_key = "your api key";
+    private final static String api_key = "your apiKey";
 
     public static String getApi_key() {
         return api_key;
