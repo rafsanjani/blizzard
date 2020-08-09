fbb65ec1baf9e39267f4eaf6f032faba44b986c0
Anthony <tonyqed@gmail.com>
2020-08-09 17:07:27 +0100

    The toolbar uses AppBarConfiguration now and no multiple fragment in back stack

--- app/src/main/java/com/example/blizzard/MainActivity.java
+++ app/src/main/java/com/example/blizzard/MainActivity.java
@@ -6,6 +6,10 @@ import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,8 +17,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
         Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        NavigationUI.setupWithNavController(toolbar, navController, appBarConfiguration);
 
     }
 
