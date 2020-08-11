28528050c29b6c1941d0479f1200d92043ccea59
kelvin <kelvinclark2k77@gmail.com>
2020-08-11 01:58:22 +0100

    Save search result to db
    Implemented a room database to save
    the queries the user makes, for
    later getting notifications when the
    data changes on the server.
    Added few dependencies.

--- app/src/main/java/com/example/blizzard/Util/ApiKeyHolder.java
+++ app/src/main/java/com/example/blizzard/Util/ApiKeyHolder.java
@@ -6,12 +6,7 @@ import com.example.blizzard.BuildConfig;
  * Created by kelvi on 8/3/2020
  */
 
-public class ApiKeyHolder {
-    public final static String API_KEY = BuildConfig.API_KEY;
-    
-}
-=======
 public  class ApiKeyHolder {
     public final static String API_KEY = BuildConfig.API_KEY;
-
 }
+
