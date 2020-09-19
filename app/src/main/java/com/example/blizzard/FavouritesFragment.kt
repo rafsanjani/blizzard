a5d533162d95f21211ee897c23a479a883999972
Kelvin Clark <46226886+lokaimoma@users.noreply.github.com>
2020-09-19 11:31:13 +0100

    Replace null assertions with null safety

--- app/src/main/java/com/example/blizzard/FavouritesFragment.kt
+++ app/src/main/java/com/example/blizzard/FavouritesFragment.kt
@@ -1,126 +1,84 @@
-package com.example.blizzard;
+package com.example.blizzard
 
-import android.os.Bundle;
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.animation.AnticipateInterpolator
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.blizzard.data.entities.WeatherDataEntity
+import com.example.blizzard.util.FavouriteFragmentAdapter
+import com.example.blizzard.viewmodel.BlizzardViewModel
+import kotlinx.android.synthetic.main.fragment_favourites.*
+import java.util.*
+import java.util.concurrent.Executors
+import java.util.concurrent.atomic.AtomicReference
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import android.os.Handler;
-import android.os.Looper;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.animation.AnticipateInterpolator;
-import android.view.animation.AnticipateOvershootInterpolator;
-import android.widget.ImageView;
-import android.widget.TextView;
-
-import com.example.blizzard.data.entities.WeatherDataEntity;
-import com.example.blizzard.data.repository.BlizzardRepository;
-import com.example.blizzard.util.FavouriteFragmentAdapter;
-import com.example.blizzard.viewmodel.BlizzardViewModel;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-import java.util.concurrent.Executors;
-import java.util.concurrent.atomic.AtomicReference;
+class FavouritesFragment : Fragment() {
+    private var adapter: FavouriteFragmentAdapter? = null
+    private var favHandler: Handler? = null
 
 
-public class FavouritesFragment extends Fragment {
-    private RecyclerView weatherRecyclerView;
-    private FavouriteFragmentAdapter adapter;
-    private Handler favHandler;
-    private ImageView ivNoData;
-    private TextView tvNoData;
-
-
-    public FavouritesFragment() {
-
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_favourites, container, false)
     }
 
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        favHandler = Handler(Looper.getMainLooper())
+        val layoutManager = LinearLayoutManager(context)
+        rv_fav.layoutManager = layoutManager
+        adapter = FavouriteFragmentAdapter(context, ArrayList())
+        rv_fav.adapter = adapter
+        initialiseAdapter()
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-
-        return inflater.inflate(R.layout.fragment_favourites, container, false);
-    }
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        favHandler = new Handler(Objects.requireNonNull(Looper.myLooper()));
-        weatherRecyclerView = view.findViewById(R.id.rv_fav);
-        ivNoData = view.findViewById(R.id.iv_no_data);
-        tvNoData = view.findViewById(R.id.tv_no_data);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
-        weatherRecyclerView.setLayoutManager(layoutManager);
-        adapter = new FavouriteFragmentAdapter(getContext(), new ArrayList<>());
-        weatherRecyclerView.setAdapter(adapter);
-        initialiseAdapter();
-    }
-
-    private void initialiseAdapter() {
-        BlizzardViewModel viewModel = new ViewModelProvider(requireActivity()).get(BlizzardViewModel.class);
-        AtomicReference<List<WeatherDataEntity>> entities = new AtomicReference<>();
-        Executors.newSingleThreadExecutor().execute(() -> {
-
-                    List<WeatherDataEntity> dataEntities = viewModel.getAllDataFromDb();
-                    List<WeatherDataEntity> favWeather = new ArrayList<>();
-
-                    for (WeatherDataEntity dataEntity : dataEntities) {
-                        if (dataEntity.getFavourite()) {
-                            favWeather.add(dataEntity);
-                        }
-                    }
-
-                    entities.set(favWeather);
-
-                    makeViewsVisible(entities);
+    private fun initialiseAdapter() {
+        val viewModel = ViewModelProvider(requireActivity()).get(BlizzardViewModel::class.java)
+        val entities = AtomicReference<List<WeatherDataEntity>>()
+        Executors.newSingleThreadExecutor().execute {
+            val dataEntities = viewModel.allDataFromDb
+            val favWeather: MutableList<WeatherDataEntity> = ArrayList()
+            for (dataEntity in dataEntities) {
+                if (dataEntity.favourite) {
+                    favWeather.add(dataEntity)
                 }
-        );
-
-    }
-
-    private void makeViewsVisible(AtomicReference<List<WeatherDataEntity>> entities) {
-        if (entities.get().size() > 0) {
-            favHandler.post(() -> {
-                adapter.insertWeatherEntities(entities.get());
-                ivNoData.setVisibility(View.INVISIBLE);
-                weatherRecyclerView.setVisibility(View.VISIBLE);
-                tvNoData.setVisibility(View.INVISIBLE);
-            });
-
-        } else {
-            favHandler.postDelayed(() -> {
-                ivNoData.setAlpha(0f);
-                tvNoData.setAlpha(0f);
-                ivNoData.animate()
-                        .alpha(1)
-                        .setDuration(100)
-                        .setInterpolator(new AnticipateInterpolator())
-                        .start();
-                tvNoData.animate()
-                        .alpha(1)
-                        .setDuration(100)
-                        .setInterpolator(new AnticipateInterpolator())
-                        .start();
-                ivNoData.setVisibility(View.VISIBLE);
-                tvNoData.setVisibility(View.VISIBLE);
-            }, 1000);
+            }
+            entities.set(favWeather)
+            makeViewsVisible(entities)
         }
     }
 
-
+    private fun makeViewsVisible(entities: AtomicReference<List<WeatherDataEntity>>) {
+        if (entities.get().isNotEmpty()) {
+            favHandler!!.post {
+                adapter!!.insertWeatherEntities(entities.get())
+                iv_no_data!!.visibility = View.INVISIBLE
+                rv_fav!!.visibility = View.VISIBLE
+                tv_no_data!!.visibility = View.INVISIBLE
+            }
+        } else {
+            favHandler!!.postDelayed({
+                iv_no_data!!.alpha = 0f
+                tv_no_data!!.alpha = 0f
+                iv_no_data!!.animate()
+                        .alpha(1f)
+                        .setDuration(100)
+                        .setInterpolator(AnticipateInterpolator())
+                        .start()
+                tv_no_data!!.animate()
+                        .alpha(1f)
+                        .setDuration(100)
+                        .setInterpolator(AnticipateInterpolator())
+                        .start()
+                iv_no_data!!.visibility = View.VISIBLE
+                tv_no_data!!.visibility = View.VISIBLE
+            }, 1000)
+        }
+    }
 }
--- app/src/main/java/com/example/blizzard/MainActivity.kt
+++ app/src/main/java/com/example/blizzard/MainActivity.kt
@@ -1,79 +1,61 @@
-package com.example.blizzard;
+package com.example.blizzard
 
-import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import androidx.work.*
+import com.example.blizzard.workers.DataUpdateWorker
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import java.util.concurrent.TimeUnit
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.NavigationUI;
-import androidx.work.Constraints;
-import androidx.work.ExistingPeriodicWorkPolicy;
-import androidx.work.NetworkType;
-import androidx.work.PeriodicWorkRequest;
-import androidx.work.WorkManager;
-
-import com.example.blizzard.workers.DataUpdateWorker;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
-import java.util.concurrent.TimeUnit;
-
-public class MainActivity extends AppCompatActivity {
-    private static final String TAG = "MainActivity";
-    public static final String WEATHER_UPDATE_CHECKER = "Weather_Update_Checker";
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
-
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupWithNavController(bottomNav, navController);
-        startWorker();
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav)
+        val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        NavigationUI.setupWithNavController(bottomNav, navController)
+        startWorker()
     }
 
-    private void startWorker() {
-        Log.d(TAG, "startWorker: Worker Manager initializing work");
-        Constraints constraints = new Constraints.Builder()
+    private fun startWorker() {
+        Log.d(TAG, "startWorker: Worker Manager initializing work")
+        val constraints = Constraints.Builder()
                 .setRequiredNetworkType(NetworkType.CONNECTED)
-                .build();
-
-        PeriodicWorkRequest request =
-                new PeriodicWorkRequest
-                        .Builder(DataUpdateWorker.class, 1, TimeUnit.HOURS)
-                        .setConstraints(constraints)
-                        .setInitialDelay(15, TimeUnit.SECONDS)
-                        .build();
-
-        WorkManager.getInstance(getApplicationContext())
-                .enqueueUniquePeriodicWork(WEATHER_UPDATE_CHECKER, ExistingPeriodicWorkPolicy.KEEP, request);
+                .build()
+        val request = PeriodicWorkRequest.Builder(DataUpdateWorker::class.java, 1, TimeUnit.HOURS)
+                .setConstraints(constraints)
+                .setInitialDelay(15, TimeUnit.SECONDS)
+                .build()
+        WorkManager.getInstance(applicationContext)
+                .enqueueUniquePeriodicWork(WEATHER_UPDATE_CHECKER, ExistingPeriodicWorkPolicy.KEEP, request)
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        val id = item.itemId
+        return if (id == R.id.action_settings) {
+            true
+        } else super.onOptionsItemSelected(item)
+    }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    companion object {
+        private const val TAG = "MainActivity"
+        const val WEATHER_UPDATE_CHECKER = "Weather_Update_Checker"
     }
 }
